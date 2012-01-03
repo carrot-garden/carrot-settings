5e44524990fc068515614359f8afc3ba1cad5574
Andrei Pozolotin <Andrei.Pozolotin@gmail.com>
2012-01-02 23:19:40 -0600

    a

--- src/test/java/bench/git/MainGit.java
+++ src/test/java/bench/git/MainGit.java
@@ -5,7 +5,6 @@ import java.io.File;
 import org.eclipse.jgit.api.CloneCommand;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.revwalk.RevTag;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 import org.eclipse.jgit.transport.CredentialsProvider;
 import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
@@ -29,38 +28,76 @@ public class MainGit {
 		logger.debug("init");
 
 		final FileRepositoryBuilder builder = new FileRepositoryBuilder();
+		builder.setGitDir(FILE);
+		builder.readEnvironment();
+		builder.findGitDir();
 
-		final Repository repo = builder.setGitDir(FILE).readEnvironment()
-				.findGitDir().build();
+		final Repository repo = builder.build();
+
+		// logger.debug("branch=" + repo.getBranch());
+		// logger.debug("state=" + repo.getRepositoryState());
+
+		switch (repo.getRepositoryState()) {
+		case BARE:
+			break;
+		case SAFE:
+			break;
+		default:
+			logger.error("wrong state");
+		}
+
+		final Git git = make();
+		logger.debug("state=" + git.getRepository().getRepositoryState());
+
+		// git.checkout().setName(Constants.MASTER).call();
+		// git.pull().setTimeout(10).call();
+
+		git.fetch().call();
+
+		git.checkout().setName("current-config").call();
+
+		// runClone();
 
 		// final Git git = Git.open(FILE);
 		// final StatusCommand status = git.status();
 		// final Status result = status.call();
 		// logger.debug("result=" + result);
 
-		testClone();
-
-		final Git git = new Git(repo);
+		// final Git git = new Git(repo);
 
 		// final RevCommit commit =
 		// git.commit().setMessage("test commit").call();
 
-		final RevTag tag = git.tag().setName("test_tag").call();
+		// final RevTag tag = git.tag().setName("test_tag").call();
+
+		// logger.debug("branch=" + repo.getBranch());
+		// logger.debug("state=" + repo.getRepositoryState());
 
 		logger.debug("done");
 
 	}
 
-	static void testClone() {
+	static Git make() {
+
+		try {
+			return makeClone();
+		} catch (final Exception e) {
+			logger.debug("clone : " + e.getMessage());
+		}
+
+		try {
+			return makeOpen();
+		} catch (final Exception e) {
+			logger.debug("open : " + e.getMessage());
+		}
+
+		return null;
+	}
+
+	static Git makeClone() throws Exception {
 
 		final CloneCommand clone = Git.cloneRepository();
-
 		clone.setBare(false);
-
-		clone.setBranch("HEAD");
-
-		clone.setCloneAllBranches(true);
-
 		clone.setDirectory(FILE).setURI(URL);
 
 		final CredentialsProvider provider = //
@@ -68,7 +105,13 @@ public class MainGit {
 
 		clone.setCredentialsProvider(provider);
 
-		clone.call();
+		return clone.call();
+
+	}
+
+	static Git makeOpen() throws Exception {
+
+		return Git.open(FILE);
 
 	}
 
